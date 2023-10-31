2cbbe508bc48c1bcee9e0fa63f73cf9be5681d54
harsh mann <97180515+harshmann10@users.noreply.github.com>
2023-10-31 23:33:22 +0530

    Update build.js

--- webapp/scripts/build.js
+++ webapp/scripts/build.js
@@ -1,48 +1,26 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-/* eslint-disable no-console */
-
-const chalk = require('chalk');
-const concurrently = require('concurrently');
-
-const {getExitCode, getPlatformCommands} = require('./utils.js');
-
-async function buildAll() {
-    console.log(chalk.inverse.bold('Building subpackages...') + '\n');
-
-    try {
-        const {result} = concurrently(
-            getPlatformCommands('build'),
-            {
-                killOthers: 'failure',
-            },
-        );
-
-        await result;
-    } catch (closeEvents) {
-        console.error(chalk.inverse.bold.red('Failed to build subpackages'), closeEvents);
-        return getExitCode(closeEvents);
-    }
-
-    console.log('\n' + chalk.inverse.bold('Subpackages built! Building web app...') + '\n');
-
-    // It's not necessary to run these commands through concurrently, but it makes the output consistent
-
-    try {
-        const {result} = concurrently([
-            {command: 'npm:build --workspace=channels', name: 'webapp', prefixColor: 'cyan'},
-        ]);
-        await result;
-    } catch (closeEvents) {
-        console.error(chalk.inverse.bold.red('Failed to build web app'), closeEvents);
-        return getExitCode(closeEvents);
-    }
-
-    console.log('\n' + chalk.inverse.bold('Web app built!'));
-    return 0;
+// Create a new directory for the build output
+const buildDir = './dist';
+if (!fs.existsSync(buildDir)) {
+  fs.mkdirSync(buildDir);
 }
 
-buildAll().then((exitCode) => {
-    process.exitCode = exitCode;
-});
+// Copy the JavaScript source files to the build directory
+const srcFiles = glob.sync('./src/*.js');
+for (const srcFile of srcFiles) {
+  fs.copyFileSync(srcFile, path.join(buildDir, srcFile));
+}
+
+// Transpile the JavaScript code to ES5
+const transpiler = require('babel-cli/bin/babel');
+transpiler.main([
+  '--presets',
+  'env',
+  '--out-dir',
+  buildDir,
+  buildDir,
+]);
+
+// Minify the JavaScript code
+const minifier = require('uglify-js');
+const minifiedCode = minifier.minify(fs.readFileSync(path.join(buildDir, 'index.js'), 'utf8')).code;
+fs.writeFileSync(path.join(buildDir, 'index.min.js'), minifiedCode);
